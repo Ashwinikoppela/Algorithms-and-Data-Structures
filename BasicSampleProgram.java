ae5b6cc94273fb368a60d5c7b0468a45efc6ca6e
Koppula <ashwini.koppula@capgemini.com>
2020-02-11 23:09:41 -0600

    Basic Sample program test

---
+++ BasicSampleProgram.java
@@ -0,0 +1,12 @@
+
+public class BasicSampleProgram {
+
+	public static void removeDuplicates(int[] array) {
+		System.out.println(array[0]);
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {12,32,33,43};
+		removeDuplicates(array);
+	}
+}
